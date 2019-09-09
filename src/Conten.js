5e35e98191450f3eae89cdbbe9c87dad3fc78cc6
phonzaza <sittiponzaza@gmail.com>
2019-09-09 08:15:47 +0700

    first

---
+++ src/Conten.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+export default class Content extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+            name:''
+        }
+    }
+    addNum = () => {
+        this.setState({
+            count:this.state.count+1
+            name:'test'
+        })
+    }
+    render() {
+        return (
+            <div>
+                {this.state.name}
+                <button onClick={this.addNum}>count:{this.state.count}</button>
+            </div>
+        );
+    }
+}
