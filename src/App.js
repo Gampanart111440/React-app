0a86abe86781313ca534a879767b9e391fd59afd
Gampanart111440 <s5935512089@phuket.psu.ac.th>
2019-09-09 09:15:13 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+const word = "Hello";
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- <p>{2+3}</p>
- </div>
- );
- }
+  render() {
+    return (
+      <div className="card">
+        Hello World
+ <p>{2 + 3}</p>
+        <CharacterCard value="h" />
+        <CharacterCard value="i" />
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+        }
+      </div>
+    );
+  }
 }
 export default App;
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -3,8 +3,8 @@ export default class CharacterCard extends Component {
     render(){
     return (
     <div>{this.props.value}</div>
-    )
+    );
     }
    }
-
+   
    
