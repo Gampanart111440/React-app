38934aac926b46cdf2b5d16155a00c8f5dc48f0c
Gampanart111440 <55080399+Gampanart111440@users.noreply.github.com>
2019-09-22 23:54:27 +0700

    :lipstick: add new result

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
-const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div className="card">
-        Hello World
- <p>{2 + 3}</p>
-        <CharacterCard value="h" />
-        <CharacterCard value="i" />
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
-        }
+      <div>
+        <WordCard value="hello" />
       </div>
     );
   }
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
-export default class WordCard extends
-    Component {
+import CharacterCard from './CharacterCard'
+import './App.css'
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
+export default class WordCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+    activationHandler = (c) => {
+        let guess = [...this.state.guess, c]
+        this.setState({ guess })
+        if (guess.length === this.state.chars.length) {
+            if (guess.join('').toString() === this.state.word) {
+                this.setState({ guess: [], completed: true })
+            } else {
+                this.setState({ guess: [], attempt: this.state.attempt + 1 })
+            }
+        }
+    }
+
     render() {
+        console.log("Word: ", this.state.word);
+        console.log("Char: ", this.state.chars);
+        console.log("Guess: ", this.state.guess);
+        console.log("Guess: ", this.state.guess.join(""));
+        console.log("---------------------------------------");
+
         return (
             <div>
-                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} />)}
+                <div>
+                    {Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                        activationHandler={this.activationHandler} />
+                    )}
+                </div>
+                <div>
+                {Array.from(this.state.guess).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                        activationHandler={this.activationHandler} />
+                    )}
+                </div>
             </div>
         );
     }
